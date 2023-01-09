433bba51397b5b640c78e3b96bb327aab8c9f7c7
anu badoni <104890769+anuragbadoni@users.noreply.github.com>
2023-01-09 12:46:12 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,13 @@ import EmailInput from "./EmailInput";
 import PhoneInput from "./PhoneInput";
 import LoginButton from "./LoginButton";
 import Footer from "./Footer";
-import { useState } from "react";
+import LoginOuterButton from "./LoginOuterButton"
+import { useState } from "react"
 
 
 
 const Sidebar = () => {
-    const [sidebar, changeSidebar] = useState(false);
+    const [sidebar, changeSidebar] = useState(true);
     const [emailOrPhone, changeEmailOrPhone] = useState("email");
 
     const crossClicked = () => {
@@ -24,7 +25,10 @@ const Sidebar = () => {
 
 
     return (
-        <div className={`absolute md:w-[600px] right-0 top-0 h-full min-w-fit shadow-2xl ${sidebar ? "hidden" : "flex"}     `}   >
+        <div className="w-screen h-screen ">
+        <LoginOuterButton Clicked={crossClicked} display={sidebar}/>
+        <div className={`absolute md:w-[600px] right-0 top-0 h-full min-w-fit shadow-2xl  ${sidebar ? "hidden" : "flex"}     `}   >
+        
             <div className="h-full pt-3 pl-4 pr-1 sm:pr-6 sm:pr-28 sm:pl-12  ">
 
 
@@ -35,6 +39,7 @@ const Sidebar = () => {
                 <LoginButton clickedEmail={changeToEmail} data={emailOrPhone} />
                 <Footer />
             </div>
+        </div>
         </div>
     );
 };
